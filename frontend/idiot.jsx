f4ecfc2a498b06959ceb584976f62c1b860579c5
Itai Farhi <itaimf@gmail.com>
2021-02-10 10:05:08 -0800

    fix bug

--- frontend/idiot.jsx
+++ frontend/idiot.jsx
@@ -3,5 +3,5 @@ import ReactDOM from "react-dom";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<h1>Welcome to Idiot!</h1>, root);
+  ReactDOM.render(<h1>Welcome to Annotatify!</h1>, root);
 });
