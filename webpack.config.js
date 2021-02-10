9ea369e5166be73ae443d2faebffcb2b2fe52d13
Itai Farhi <itaimf@gmail.com>
2021-02-10 10:09:22 -0800

    change titles

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './frontend/idiot.jsx',
+  entry: './frontend/annotatify.jsx',
   output: {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: 'bundle.js'
