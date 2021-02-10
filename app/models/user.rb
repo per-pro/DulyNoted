82ec8fc44a26cb6ae5d131c7f6ed3330b3bb527e
Itai Farhi <itaimf@gmail.com>
2021-02-10 10:02:12 -0800

    fix bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
-    vaidates :password_digest, presence: true
+    validates :password_digest, presence: true
 
 end
