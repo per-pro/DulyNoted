e45665c930686d0931cc28280bb2e621c041f00c
Itai Farhi <itaimf@gmail.com>
2022-02-17 03:41:10 -0800

    finish reducers

---
+++ frontend/reducers/comments_reducer.js
@@ -0,0 +1,13 @@
+import { RECEIVE_COMMENTS } from "../actions/comment_actions";
+
+const commentsReducer = (state = {}, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+    case RECEIVE_COMMENTS:
+      return action.comments;
+    default:
+      return state;
+  }
+};
+
+export default commentsReducer;
--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './users_reducer';
+import songsReducer from './songs_reducer';
+import commentsReducer from './comments_reducer'
 
 const entitiesReducer = combineReducers({
-  users: userReducer
+  users: userReducer,
+  songs: songsReducer,
+  comments: commentsReducer
 });
 
 export default entitiesReducer;
