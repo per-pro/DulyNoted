6b1d770ba084cf1935473d6429b3aaa80e3296b1
Itai Farhi <itaimf@gmail.com>
2022-02-17 02:26:16 -0800

    song controller

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,2 +1,21 @@
 class Api::SongsController < ApplicationController
+
+    def index
+        @songs = Song.all 
+    end
+
+    def new 
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.new(title: "...", author: "...", content: "...")
+
+        if @song.save
+            redirect_to @song
+        else 
+            render :new, status: :error
+        end 
+    end 
+
 end
