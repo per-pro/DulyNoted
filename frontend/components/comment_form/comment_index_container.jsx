8b29db168f160810a4fc22c95745e62c66879fcb
Itai Farhi <itaimf@gmail.com>
2022-02-22 09:38:00 -0800

    create containers

--- frontend/components/comment_form/comment_index_container.jsx
+++ frontend/components/comment_form/comment_index_container.jsx
@@ -1,6 +1,18 @@
 import CommentIndex from "../comment_index.jsx";
 
-//...
+const mapStateToProps = (state, ownProps) => ({
+  errors: state.errors.comment,
+  comment: {
+      body: '',
+      startIndex: ownProps.startIndex,
+      endIndex: ownProps.endIndex,
+      songId: ownProps.match.params.songId,
+  },
+  songs: state.entities.songs,
+  formType: 'Create',
+  selectComment: ownProps.selectComment,
+  clearComment: ownProps.clearComment,
+});
 
 export default connect(
   mapStateToProps,
--- frontend/components/song_form/song_index_container.jsx
+++ frontend/components/song_form/song_index_container.jsx
@@ -1,10 +1,22 @@
 import SongIndex from "./song_index.jsx";
 
 const mapStateToProps = (state, ownProps) => ({
-    errors: state.errors.session,
-    formType: 'login'
+  song: state.entities.tracks[ownProps.match.params.trackId],
+  artists: state.entities.artists,
+  comments: Object.values(state.entities.comments).sort((a,b) => {
+      if (a.startIndex < b.startIndex) {
+          return -1
+      } else {
+          return 1
+      }
+  }),
+  
 });
 
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  requestSong: (songId) => dispatch(requestSong(songId)),
+
+});
 
 export default connect(
   mapStateToProps,
