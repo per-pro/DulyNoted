504583f10ba51557be39baa77e6bfd9df6089c3f
Itai Farhi <itaimf@gmail.com>
2022-03-18 02:54:09 -0700

    finally got app to load!

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,7 +6,6 @@ import {
   Link,
   HashRouter
 } from 'react-router-dom';
-import GreetingContainer from "./greeting/greeting_container";
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
