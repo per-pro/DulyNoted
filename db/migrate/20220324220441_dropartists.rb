eff640496b63b6c1a4fb0d7bc71f198c6aa68d68
Itai Farhi <itaimf@gmail.com>
2022-03-24 15:05:42 -0700

    drop artists table

---
+++ db/migrate/20220324220441_dropartists.rb
@@ -0,0 +1,5 @@
+class Dropartists < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :artists
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_24_215020) do
+ActiveRecord::Schema.define(version: 2022_03_24_220441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "artists", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name", null: false
