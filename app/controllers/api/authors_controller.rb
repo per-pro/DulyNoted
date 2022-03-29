0d9bf564917f20d18c6e92d0a70f85866a2b81e3
Itai Farhi <itaimf@gmail.com>
2022-03-29 00:45:41 -0700

    define actions on authors controller

--- app/controllers/api/authors_controller.rb
+++ app/controllers/api/authors_controller.rb
@@ -1,2 +1,23 @@
 class Api::AuthorsController < ApplicationController
+
+    def index
+        @authors = Author.all
+    end
+
+    def new 
+        @author = Author.new
+    end
+
+    def create
+        @author = Author.new(name: "...")
+
+        if @comment.save
+            redirect_to @author
+        else
+            render :new, status: :error
+        end
+    end
+
+    # should I add other routing actions? I can add them as they become necessary
+
 end
