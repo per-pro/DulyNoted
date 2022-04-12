6b9d8f334e45992dc554b6fe38aff446c83226d6
Itai Farhi <itaimf@gmail.com>
2022-04-11 22:23:39 -0700

    choose authors to include in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,26 @@ Comment.destroy_all
 Text.destroy_all
 User.destroy_all
 
+# Authors
+spinoza = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+kant = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+fichte = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+schelling = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+maimon = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+hegel = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+marx = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+lenin = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+freud = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+trotsky = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+foucault = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+marcuse = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+ilyenkov = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+rose = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+chitty = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+zukofsky = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+ducasse = Author.create({ name, alive, birth_date, category, created_at, updated_at, death_date, description })
+# ok_go = Artist.create({ name: 'OK Go', bio: "Formed in Chicago in 1998, OK Go; Damian Kulash, Dan Konopka, Timothy Nordwind, and Andy Ross, is an indie alternative rock band, known for their eye-catching, low-budget music videos.
+
+# In 2002, as Damian Kulash, Timothy Nordwind, Dan Konopka, and Andy Duncan, they released their self-titled debut, to generally positive claim, and has since attained a cult following among fans and critics alike.
+
+# In 2005, as Andy Duncan left, “Oh No” was released to more positive reviews, as Andy Ross joined, and “Here It Goes Again” reached #38 on the Billboard Hot 100."})
