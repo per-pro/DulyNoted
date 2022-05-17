ff52d99c39ac32395a8f30efdd4b3aa55c99807a
Itai Farhi <itaimf@gmail.com>
2022-05-17 10:44:40 -0700

    add third story, might add more if I can think of a good one

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
                 <div className="news">
                     <h2>New Translation of Dante's Purgatorio Incorporates Contemporary Jargon</h2>
                     <h3>Its use of selfies and references to pop culture has the literary world buzzing. </h3>
+                    <h4>by Jebediah Brownsworthy</h4>
                     <img src="https://i0.wp.com/hyperallergic-newspack.s3.amazonaws.com/uploads/2021/11/PURGATORIO-scaled.jpeg?resize=1200%2C1791&quality=100&ssl=1" 
                     height={400}
                     width={300}/>
@@ -18,12 +19,18 @@ const Home = () => {
                 <div className="news">
                     <h2>The Believer Magazine returns to McSweeney's</h2>
                     <h3>Magazine was previously owned by Paradise media and UNLV</h3>
+                    <h4>by Adele Delacruz</h4>
                     <img src="https://hyoom.com/wp-content/uploads/2018/05/McSweeneys.jpg"
                     height={400}
                     width={300} />
                 </div>
                 <div className="news">
-
+                    <h2>The University at Buffalo Receives Federal Grant for Joyce Museum</h2>
+                    <h3>Collection includes censored first edition of A Farewell to Arms with vulgarity reinserted by Hemingway</h3>
+                    <h4>by Adele Delacruz</h4>
+                    <img src="https://i.pinimg.com/474x/eb/79/74/eb79749d5c350054817f46b8db4fefc5--james-joyce-james-darcy.jpg" 
+                    height={400}
+                    width={300} />
                 </div>
             </div>
         </div>
