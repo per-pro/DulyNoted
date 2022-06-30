f77615348a5f6cdda850deb4e868b371b53db30b
Itai Farhi <itaimf@gmail.com>
2022-06-30 14:07:33 -0700

    add feature buttons

--- frontend/components/feature/feature.jsx
+++ frontend/components/feature/feature.jsx
@@ -3,7 +3,33 @@ import { Link } from 'react-router-dom';
 
 
 const Feature = () => {
+    const featureGroup = () => {
+        <hgroup classname="feature-group">
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">FEATURED</button>
+            </div>
 
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">CHARTS</button>
+            </div>
+
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">VIDEOS</button>                
+            </div>
+
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">PROMOTE</button>            
+            </div>
+
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">FORUMS</button>            
+            </div>
+
+            <div classname="button">
+                <button onclick="myFunction()" className="dropbtn">FORUMS</button>            
+            </div>
+        </hgroup>
+    }
  };
  
  export default Feature;
