28412f937f04ab6df98b9c669cd939fe67385a9e
Itai Farhi <itaimf@gmail.com>
2023-03-30 11:11:48 -0700

    rename dash to ui, brainstorm fonts

--- frontend/components/dashboard/dashboard.jsx
+++ frontend/components/dashboard/dashboard.jsx
@@ -9,10 +9,12 @@ const Dashboard = () => {
     // 2. index of my texts
     // 3. update existing texts
     // 4. delete texts
-
+//5.header: convergence? hi melody? miriam libre? passions conflict? quintessential? red rose? shalimar? vesper libre?
+//6.body: dotgothic? erica one? secular one? ultra? unlock?
+//7.leaning towards h:red rose, b:mix of dotgothic and erica one
 
     return (
-        <div className="dash-container">
+        <div className="ui-container">
             <main>
                 <p>
                     Hello, I am text you can only see if you have logged in.
